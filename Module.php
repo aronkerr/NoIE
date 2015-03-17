148567c2d2a23833309f4a0dbf4add8bb129ea42
Aron Kerr <aronkerr@gmail.com>
2015-03-17 14:45:33 -0700

    Init commit

---
+++ Module.php
@@ -0,0 +1,51 @@
+<?php
+namespace NoIe;
+
+use Application\Model\Browser;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\MvcEvent;
+use Zend\Console\Console;
+
+class Module implements AutoloaderProviderInterface
+{
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    // if we're in a namespace deeper than one level we need to fix the \ in the path
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/', __NAMESPACE__),
+                ),
+            ),
+        );
+    }
+
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        if (!Console::isConsole()) {
+            $eventManager = $e->getApplication()->getEventManager();
+            $sharedManager = $eventManager->getSharedManager();
+
+            // Check the browser and redirect if it is IE or old
+            $sharedManager->attach('*', MvcEvent::EVENT_ROUTE, function ($e) {
+                $serviceManager = $e->getApplication()->getServiceManager();
+                $browserCheck = new Browser();
+
+                $browser = $browserCheck->getBrowser();
+                if ($browser['name'] == 'Internet Explorer') {
+                    $response = $e->getResponse();
+                    $response->setStatusCode(406);
+
+                    $viewModel = $e->getViewModel();
+                    $viewModel->setTemplate('error/406');
+                }
+            });
+        }
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'view_manager' => array(
+        'template_map' => array(
+            'error/406'               => __DIR__ . '/../view/error/406.phtml'
+        )
+    )
+);
